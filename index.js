6e253ef6960465bcf17ebfaf6df0f3cd210bfedc
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-24 07:56:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const commandsMain = require('./commands/main');
+
+app.use('/commands/main', commandsMain);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
