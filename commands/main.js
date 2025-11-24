d3fb0a87eff96c6bc8e07fda3e527f4dd4aaca3b
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-24 08:00:07 +0100

    Update main.js

--- commands/main.js
+++ commands/main.js
@@ -4,7 +4,6 @@ const router = express.Router();
 router.post('/', (req, res) => {
   const data = req.body;
 
-  // for now just send back the data you received
   res.json({
     message: 'POST received successfully.',
     received: data
