56d22c2cf697c7f63d0db375c6d52b36025f846b
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-24 13:31:28 +0100

    Create editMessage.js

---
+++ commands/editMessage.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { botToken, channelID, messageID, body } = req.body;
+
+  if (!botToken || !channelID || !messageID || !body) return res.sendStatus(400);
+
+  try {
+    await fetch(`https://discord.com/api/v10/channels/${channelID}/messages/${messageID}`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bot ${botToken}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
