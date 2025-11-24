9634c9fd95d2657f511b97774a9fd485e014394e
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-24 07:51:44 +0100

    Update main.js

--- commands/main.js
+++ commands/main.js
@@ -1,1 +1,14 @@
-e
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const data = req.body;
+
+  // for now just send back the data you received
+  res.json({
+    message: 'POST received successfully.',
+    received: data
+  });
+});
+
+module.exports = router;
