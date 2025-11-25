442e18b79ef2ecfea3925f083d4897be9aba9fde
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-25 08:56:35 +0100

    Update getData.js

--- jsonData/getData.js
+++ jsonData/getData.js
@@ -4,16 +4,22 @@ const router = express.Router();
 
 router.get('/activity-icon', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'activityIcon.json'))});
+
 router.get('/bar-icon', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'barIcon.json'))});
+
 router.get('/rank-1', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'rank1.json'))});
+
 router.get('/rank-2', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'rank2.json'))});
+
 router.get('/rank-3', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'rank3.json'))});
+
 router.get('/rank-4', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'rank4.json'))});
+
 router.get('/rank-role', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'rankRole.json'))});
 
