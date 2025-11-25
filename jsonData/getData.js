ba78aedd7e4428d61c594c33f77369c47763ab17
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-25 08:02:51 +0100

    Update getData.js

--- jsonData/getData.js
+++ jsonData/getData.js
@@ -10,4 +10,20 @@ router.get('/bar-icon', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'barIcon.json'));
 });
 
+router.get('/rank-1', (req, res) => {
+res.sendFile(path.join(__dirname, 'data', 'rank1.json'));
+});
+
+router.get('/rank-2', (req, res) => {
+res.sendFile(path.join(__dirname, 'data', 'rank2.json'));
+});
+
+router.get('/rank-3', (req, res) => {
+res.sendFile(path.join(__dirname, 'data', 'rank3.json'));
+});
+
+router.get('/rank-4', (req, res) => {
+res.sendFile(path.join(__dirname, 'data', 'rank4.json'));
+});
+
 module.exports = router;
