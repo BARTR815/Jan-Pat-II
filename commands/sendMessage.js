1b6710849cd887a20a75cafc286676fdf7dfd2f1
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-25 16:46:25 +0100

    Update sendMessage.js

--- commands/sendMessage.js
+++ commands/sendMessage.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const fetch = require('node-fetch');
+const express = require("express");
+const fetch = require("node-fetch");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-  const { botToken, channelID, body } = req.body;
-
-  if (!botToken || !channelID || !body) return res.sendStatus(400);
-
-  try {
-    await fetch(`https://discord.com/api/v10/channels/${channelID}/messages`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bot ${botToken}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    });
-
-    res.sendStatus(200);
-  } catch {
-    res.sendStatus(500);
-  }
-});
-
+router.post("/", async (req, res) => {
+	const { botToken, channelID, messageID } = req.body;
+	if ( !botToken || !channelID || !messageID ) {
+		return res.status(400).json({"error":"Incomplete Body"})
+	}
+	try {
+		const response = await fetch (`https://discord.com/api/v10/channels/${channelID}/messages/${messageID}`, {
+			"method": "GET",
+			"headers": {
+				"Authorization": `Bot ${botToken}`,
+				"Content-Type": "application/json"
+			},
+		});
+		if (!response.ok) {
+			const errorResponse = await response.json().catch(() => null);
+			return res.status(response.status).json({"error":"Discord API Error","details":errorResponse});
+		}
+		
+		const messageData = await response.json();
+		res.status(200).json(messageData);
+	} catch {
+		res.status(500).json({"error":"Server Error"})
+	}
+	});
+	
 module.exports = router;
