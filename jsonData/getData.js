6f4d8b49928f306d5a424b5a4374eed0a991d1b6
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-25 08:00:58 +0100

    Update getData.js

--- jsonData/getData.js
+++ jsonData/getData.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+router.get('/activity-icon', (req, res) => {
+res.sendFile(path.join(__dirname, 'data', 'activityIcon.json'));
+});
+
 router.get('/bar-icon', (req, res) => {
 res.sendFile(path.join(__dirname, 'data', 'barIcon.json'));
 });
