d61de8a4211232b83e46ca6003856b6f90b666b5
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-25 08:59:39 +0100

    Update getData.js

--- jsonData/getData.js
+++ jsonData/getData.js
@@ -3,27 +3,27 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/activity-icon', (req, res) => {
-res.sendFile(path.join(__dirname, 'data', 'activityIcon.json'));
+ res.sendFile(path.join(__dirname, 'data', 'activityIcon.json'));
 });
 router.get('/bar-icon', (req, res) => {
-res.sendFile(path.join(__dirname, 'data', 'barIcon.json'));
+ res.sendFile(path.join(__dirname, 'data', 'barIcon.json'));
 });
 
 router.get('/rank-1', (req, res) => {
-res.sendFile(path.join(__dirname, 'data', 'rank1.json'));
+ res.sendFile(path.join(__dirname, 'data', 'rank1.json'));
 });
 router.get('/rank-2', (req, res) => {
-res.sendFile(path.join(__dirname, 'data', 'rank2.json'));
+ res.sendFile(path.join(__dirname, 'data', 'rank2.json'));
 });
 router.get('/rank-3', (req, res) => {
-res.sendFile(path.join(__dirname, 'data', 'rank3.json'));
+ res.sendFile(path.join(__dirname, 'data', 'rank3.json'));
 });
 router.get('/rank-4', (req, res) => {
-res.sendFile(path.join(__dirname, 'data', 'rank4.json'));
+ res.sendFile(path.join(__dirname, 'data', 'rank4.json'));
 });
 
 router.get('/rank-role', (req, res) => {
-res.sendFile(path.join(__dirname, 'data', 'rankRole.json'));
+ res.sendFile(path.join(__dirname, 'data', 'rankRole.json'));
 });
 
 module.exports = router;
