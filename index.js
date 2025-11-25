84a0e0a9e0b42b899c39c3224df954f22ae73da4
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-25 17:05:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const path = require('path');
+const express = require("express");
+const fetch = require("node-fetch");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2137;
 
 app.use(express.json());
 
-const sendMessage = require('./commands/sendMessage.js');
-const editMessage = require('./commands/editMessage.js');
-const getData = require('./jsonData/getData.js');
+const sendMessage = require("./commands/sendMessage.js");
+const editMessage = require("./commands/editMessage.js");
+const getData = require("./jsonData/getData.js");
 
-app.use('/commands/send-message', sendMessage);
-app.use('/commands/edit-message', editMessage);
-app.use('/json-data/get-data', getData);
+app.use("/commands/send-message", sendMessage);
+app.use("/commands/edit-message", editMessage);
+app.use("/json-data/get-data", getData);
 
-app.get('/', (req, res) => {
-  res.send('JP2');
+app.get("/", (req, res) => {
+	res.send("JP2");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
