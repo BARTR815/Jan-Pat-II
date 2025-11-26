2ae81bbfc07c1ca0445c19b0264e5f96e6c385c0
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-26 17:51:29 +0100

    Update jsonParse.js

--- commands/jsonParse.js
+++ commands/jsonParse.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
-molib
+
+router.post("/", (req, res) => {
+	const jsonParse = req.body
+	res.status(200).json(jsonParse);
+});
+
 module.exports = router;
