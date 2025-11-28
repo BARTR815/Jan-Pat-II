44d3bd5a9cc949d2af518d43bfe686d31484a237
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-28 10:57:43 +0100

    Update getData.js

--- jsonData/getData.js
+++ jsonData/getData.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const router = express.Router();
 
-router.get('/activity-icon', (req, res) => {
- res.sendFile(path.join(__dirname, 'data', 'activityIcon.json'));
+router.get("/activity-icon", (req, res) => {
+ res.sendFile(path.join(__dirname, "data", "activityIcon.json"));
 });
-router.get('/bar-icon', (req, res) => {
- res.sendFile(path.join(__dirname, 'data', 'barIcon.json'));
-});
-
-router.get('/rank-1', (req, res) => {
- res.sendFile(path.join(__dirname, 'data', 'rank1.json'));
-});
-router.get('/rank-2', (req, res) => {
- res.sendFile(path.join(__dirname, 'data', 'rank2.json'));
-});
-router.get('/rank-3', (req, res) => {
- res.sendFile(path.join(__dirname, 'data', 'rank3.json'));
-});
-router.get('/rank-4', (req, res) => {
- res.sendFile(path.join(__dirname, 'data', 'rank4.json'));
+router.get("/bar-icon", (req, res) => {
+ res.sendFile(path.join(__dirname, "data", "barIcon.json"));
 });
 
-router.get('/rank-role', (req, res) => {
- res.sendFile(path.join(__dirname, 'data', 'rankRole.json'));
+router.get("/rank-1", (req, res) => {
+ res.sendFile(path.join(__dirname, "data", "rank1.json"));
+});
+router.get("/rank-2", (req, res) => {
+ res.sendFile(path.join(__dirname, "data", "rank2.json"));
+});
+router.get("/rank-3", (req, res) => {
+ res.sendFile(path.join(__dirname, "data", "rank3.json"));
+});
+router.get("/rank-4", (req, res) => {
+ res.sendFile(path.join(__dirname, "data", "rank4.json"));
+});
+
+router.get("/rank-role", (req, res) => {
+ res.sendFile(path.join(__dirname, "data", "rankRole.json"));
 });
 
 module.exports = router;
