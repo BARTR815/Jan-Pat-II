72dc41176e2b7ed6bc918c2627faa54de33f93bb
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2025-11-29 22:50:14 +0100

    Delete commands/getISOTimestamp.js

--- commands/getISOTimestamp.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-	const iso = new Date().toISOString();
-	res.status(200).json({"timestamp":`${iso}`});
-});
-
-module.exports = router;
