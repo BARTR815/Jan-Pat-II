a0f9d6bf0bd148ad5561a6244093189500dbc6fc
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2026-01-16 19:40:41 +0100

    Update

--- commands/baseDecode.js
+++ commands/baseDecode.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-	const encode = req.body.content
+	const decode = req.body.content
 	decodedContent = atob(decode)
 	res.status(200).json(decodedContent);
 });
