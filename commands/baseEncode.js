baa2bad89a35cb4a034c6d7e6900479b885f165c
BARTR815 <112405518+BARTR815@users.noreply.github.com>
2026-01-16 19:45:38 +0100

    Update

--- commands/baseEncode.js
+++ commands/baseEncode.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 router.post("/", (req, res) => {
 	const encode = req.body.content
-	encodedContent = btoa(encode)
-	res.status(200).json(encodedContent);
+	const encodedContent = btoa(encode);
+	res.status(200).json({content:`${encodedContent}`});
 });
 
 module.exports = router;
